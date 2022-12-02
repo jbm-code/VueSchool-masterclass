8c3cc5b3f028a141048b67c1b3ca7f40357e82fb
jbm <jbmariage@mailbox.org>
2022-12-02 15:50:39 +0100

    first commit

---
+++ src/components/PageHome.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>Hello from page home</div>
+    <div 
+        v-for="thread of threads" :key="thread.id"> 
+        <h2>{{thread.title}}</h2>  
+        <div v-for="postId of thread.posts" :key="postId">
+            <p>{{posts.find(p => p.id === postId).text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import sourceData from "@/data.json"
+
+export default {
+    data () {
+        return {
+            threads: sourceData.threads,
+            posts: sourceData.posts,
+            
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+
+</style>
