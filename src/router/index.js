2ddc6502a9bcec1a6fd91fb1f20d42f67d8de23d
jbm <jbmariage@mailbox.org>
2023-01-11 09:03:46 +0100

    creating not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import PageHome from "@/components/PageHome";
 import PageThreadShow from "@/components/PageThreadShow";
+import PageNotFound from "@/components/PageNotFound";
 import { createRouter, createWebHistory } from "vue-router";
+import sourceData from "@/data.json";
 
 const routes = [
   {
@@ -13,6 +15,30 @@ const routes = [
     name: "ThreadShow",
     component: PageThreadShow,
     props: true,
+    beforeEnter(to, from, next) {
+      //check if thread exists
+      const threadExists = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+      //if exists continue
+      if (threadExists) {
+        return next();
+      } else {
+        next({
+          name: "NotFound",
+          params: { pathMatch: to.path.substring(1).split("/") },
+          //preserve existing query and hash
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+      //if doesn't exist redirect to not found
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: PageNotFound,
   },
 ];
 
