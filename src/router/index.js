70b0a83d491c018204cc9ef9212c2e42398e6f54
jbm <jbmariage@mailbox.org>
2023-01-16 13:49:50 +0100

    Reorganizing the Page Components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import PageHome from "@/components/PageHome";
-import PageThreadShow from "@/components/PageThreadShow";
-import PageNotFound from "@/components/PageNotFound";
+import PageHome from "@/pages/PageHome";
+import PageThreadShow from "@/pages/PageThreadShow";
+import PageNotFound from "@/pages/PageNotFound";
 import { createRouter, createWebHistory } from "vue-router";
 import sourceData from "@/data.json";
 
